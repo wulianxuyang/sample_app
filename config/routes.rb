8c18abb6de27a8596df3010f81d02cd6f68405a6
xuyang <xuyang@ktjr.com>
2016-05-17 11:32:42 +0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   root to: 'static_pages#home'
+
+  get '/signup', to: 'users#new'
   get '/help', to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
