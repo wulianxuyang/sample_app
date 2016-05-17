12849d547eac0a67b36772d32562f7e7374efa87
xuyang <xuyang@ktjr.com>
2016-05-17 11:04:17 +0800

    修改路由

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 SampleApp::Application.routes.draw do
   root to: 'static_pages#home'
-  match '/help', to: 'static_pages#help', via: 'get'
-  match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
+  get '/help', to: 'static_pages#help'
+  get '/about',   to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
